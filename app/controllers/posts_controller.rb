1f7f114d5f4be0e6b2d162689ae9e36fc8dd7f18
slumfrog <kishan.anil@gmail.com>
2019-07-18 15:12:10 +0100

    Done.

---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,6 @@
+class PostsController < ActionController::Base
+    def show
+        @post = Post.find(params[:id])
+    end
+end
+  
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  # get 'posts/:id', to: 'posts#show' replace this with
+  resources :posts, only: :show
+ 
+
 end
